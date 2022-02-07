Bugfix backprop for linear layer and add sigmoid activation layer

neurgoo/layers/linear.py
@@ -3,6 +3,7 @@
 from typing import Optional, Union
 
 import numpy as np
+from loguru import logger
 
 from .._base import AbstractLayer, OptimParam
 from ..structures import Shape, Tensor
@@ -73,8 +74,13 @@ class Linear(AbstractLayer):
 
     def _initialize_default(self) -> None:
         self.W.val = np.random.randn(self.in_features, self.num_neurons)
-        if self.use_bias:
-            self.b.val = np.random.randn(self.num_neurons)
+        self.b.val = (
+            np.random.randn(self.num_neurons)
+            if self.use_bias
+            else np.zeros(
+                self.num_neurons,
+            )
+        )
 
     def feed_forward(self, X: Tensor) -> Tensor:
         """
@@ -101,6 +107,9 @@ class Linear(AbstractLayer):
         # cache the input for backprop
         self._input_cache = X
         z = X @ self.W.val
+
+        # this improves performance also
+        # instead of adding "zero" biases to z
         if self.use_bias:
             z = z + self.b.val
         return z
@@ -120,12 +129,18 @@ class Linear(AbstractLayer):
             - find gradient of input grad_accum wrt input X to the layer
 
         """
+        if self.debug:
+            logger.debug(f"Input cache shape => {self._input_cache.shape}")
+
         if self.trainable:
-            self.W.grad = self._input_cache @ grad_accum
+            # Should be of the same shape as W
+            self.W.grad = self._input_cache.T @ grad_accum
             self.b.grad = np.sum(grad_accum, axis=0, keepdims=True)
 
         # this will be used as a new "grad_accum"
         # in the previous layer  (n-1)
+        # wrt input
+        # See: how backprop works!
         return grad_accum @ self.W.val.T
 
     @property

tests/test_layer.py
@@ -5,27 +5,36 @@ import sys
 sys.path.append("../neurgoo/")
 
 import numpy as np
+from loguru import logger
 
 from neurgoo.api import Linear
+from neurgoo.layers.activations import Sigmoid
 
 
 def main():
     X = np.random.randn(42, 12)
 
-    print("-" * 10)
-    print("Forward pass test!")
-    dense = Linear(num_neurons=3, in_features=12)
-    print(dense)
+    logger.debug("-" * 10)
+    logger.debug("Forward pass test!")
+    dense = Linear(num_neurons=3, in_features=12, debug=True)
+    logger.debug(dense)
+    logger.debug(f"W shape => {dense.W.shape}")
+    logger.debug(f"b shape => {dense.b.shape}")
+    logger.debug(f"Initial W Grad => {dense.W.grad}")
     out = dense.feed_forward(X)
-    print(out.shape)
-    print(f"Initial W Grad => {dense.W.grad}")
-    print("-" * 10)
+    logger.debug(f"Out shape => {out.shape}")
+    logger.debug("-" * 10)
 
-    print("-" * 10)
-    print("Backward pass test!")
-    grad = np.random.randn(*dense.W.shape)
+    logger.debug("-" * 10)
+    logger.debug("Backward pass test!")
+    grad = np.random.randn(42, 3)
     grad_accum = dense.backpropagate(grad)
-    print(f"Resultant W Grad shape => {dense.W.grad.shape}")
+    logger.debug(f"W Grad shape => {dense.W.grad.shape}")
+    logger.debug(f"b Grad shape => {dense.b.grad.shape}")
+    logger.debug(f"New grad_accum shape => {grad_accum.shape}")
+    logger.debug("-" * 10)
+
+    wnew = dense.W.val - dense.W.grad
 
 
 if __name__ == "__main__":