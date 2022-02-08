1bc5cd8d97dbb63e79188ce6cd4c1d51bd79c252
NISH1001 <nishpantha@gmail.com>
2022-02-07 20:50:57 -0600

    Add ReLU and LeakyReLU activations
    See `neurgoo.layers.activations.ReLU`.

--- neurgoo/layers/activations.py
+++ neurgoo/layers/activations.py
@@ -15,6 +15,31 @@ class Sigmoid(Activation, ActivationLayer):
         return y * (1 - y)
 
 
+class ReLU(Activation, ActivationLayer):
+    def __call__(self, x: Tensor) -> Tensor:
+        return np.where(x >= 0, x, 0)
+
+    def gradient(self, x: Tensor) -> Tensor:
+        return np.where(x >= 0, 1, 0)
+
+
+class LeakyReLU(Activation, ActivationLayer):
+    def __init__(self, leak: float = 1e-5, name=None, debug: bool = False) -> None:
+        super().__init__(name=name, debug=debug)
+
+        if not isinstance(leak, float):
+            raise TypeError(
+                f"Invalid type for leak={leak}. Expected float. Got {type(leak)}"
+            )
+        self.leak = leak
+
+    def __call__(self, x: Tensor) -> Tensor:
+        return np.where(x > 0, x, self.leak)
+
+    def gradient(self, x: Tensor) -> Tensor:
+        return np.where(x > 0, 1, self.leak)
+
+
 def main():
     pass
 
