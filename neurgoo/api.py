b78cf7c54d698f1a31d03a9f84da016c360dbd46
NISH1001 <nishpantha@gmail.com>
2022-02-17 06:50:03 -0600

    Update neurgoo.api with relevant public interfaces
    `neurgoo.api` acts as an entrypoint to the library.

--- neurgoo/api.py
+++ neurgoo/api.py
@@ -1,3 +1,7 @@
-from .layers.activations import Sigmoid
+from .layers.activations import LeakyReLU, ReLU, Sigmoid, Softmax
 from .layers.linear import Identity, Linear
-from .structures import Tensor
+from .losses import CrossEntropyLoss, MeanSquaredError
+from .models import DefaultNNModel
+from .optimizers import SGD
+from .structures import Tensor, TensorArray
+from .trainer import DefaultModelTrainer
