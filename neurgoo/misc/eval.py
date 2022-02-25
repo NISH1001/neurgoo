3dc541f8b8101e9e91977a20517376c612dc87e8
NISH1001 <nishpantha@gmail.com>
2022-02-25 09:48:36 -0600

    Improve training tracker and plotter
    Now all the model trainer will return a dictionary that tracks
    train/val/test metrics for each epoch like loss and accuracy. We use
    `neurgoo.misc.eval.EvalData` dataclass to store info  about each epoch.
    
    Additionally, `neurgoo.misc.plot_utils.plot_history` is added.

--- neurgoo/misc/eval.py
+++ neurgoo/misc/eval.py
@@ -1,3 +1,6 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
 from typing import Sequence
 
 import numpy as np
@@ -9,16 +12,44 @@ def convert_prob_to_label(y):
     return np.argmax(y, axis=1).reshape((len(y), 1))
 
 
+@dataclass
+class EvalData:
+    epoch: int
+    accuracy: float
+    precision: float = -1
+    recall: float = -1
+    loss: float = np.nan
+
+    @classmethod
+    def default_empty(cls) -> EvalData:
+        return cls(epoch=-1, accuracy=-1, precision=-1, recall=-1)
+
+
 class Evaluator:
+    """
+    A helper class to evaluate predictions.
+    """
+
     def __init__(self, num_classes: int = 10):
         assert isinstance(num_classes, int)
         self.num_classes = num_classes
 
-    def calculate_accuracy(
+    def calculate_accuracy_from_labels(
         self, target_label: Sequence[int], predicted_label: Sequence[int]
     ):
         matched = np.sum(target_label == predicted_label)
         return matched / len(target_label)
+
+    def calculate_accuracy(self, target: Tensor, predicted: Tensor) -> float:
+        """
+        Args:
+            target: ``Tensor`` or ``np.ndarray``
+                Target y values, one-hot-encoded.
+                Of the dimension (Nxm), where N is batch_size, m is num_classes
+        """
+        return np.sum(np.argmax(predicted, axis=1) == np.argmax(target, axis=1)) / len(
+            target
+        )
 
     def calculate_metrics(
         self, target_label: Sequence[int], predicted_label: Sequence[int]
